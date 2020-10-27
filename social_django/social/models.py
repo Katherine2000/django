d7c882d76345dc4c7441865cbc11fe9a73c60576
Katherine Camacho <katy200021@gmail.com>
2020-10-27 18:04:31 -0500

    ultimos cambios

--- social_django/social/models.py
+++ social_django/social/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete= models.CASCADE)
 	image = models.ImageField(default='batman.png')
-	
+
 
 
 	def __str__(self):
